4fe4debd0a1d4c7c4b44d5a4829bf9993112def8
amjido-01 <youndsadeeq10@gmail.com>
2025-01-20 13:36:31 +0000

    commit

--- app/(routes)/(private)/notifications/page.tsx
+++ app/(routes)/(private)/notifications/page.tsx
@@ -30,7 +30,7 @@ interface Notification {
   productName: string
   message: string
   createdAt: string
-  sent: Boolean
+  sent: boolean
 
 }
 
@@ -60,7 +60,7 @@ const NotificationCard: React.FC<{ notification: Notification; onDelete: (id: st
 
 const NotificationsPage = () => {
   const [userNotifications, setUserNotifications] = useState<Notification[]>([])
-  const [isLoading, setIsLoading] = useState<Boolean>(true)
+  const [isLoading, setIsLoading] = useState<boolean>(true)
   const { logout } = useAuthStore()
   const router = useRouter();
 
